6c079adeda24061aee4623269c0d341579b4e493
Rick <rmt1998@hotmail.nl>
2018-12-31 21:03:57 +0100

    Version 2.0

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 API_URL = 'https://db.ygoprodeck.com/api/v2/cardinfo.php'
 PIC_URL = 'https://ygoprodeck.com/pics/'
-
+PIC_SMALL_URL = 'https://ygoprodeck.com/pics_small/'
+MAX_ITEMS = 50
 
 /**
  * Search for a keyword in the ygoprodeck database
@@ -8,19 +9,14 @@ PIC_URL = 'https://ygoprodeck.com/pics/'
 function search(keyword) {
     return new Promise((resolve, reject) => {
         const https = require('https');
-
         let query = '?fname=' + keyword;
         let url = API_URL + query;
-
         let req = https.get(url, (res) => {
             let body = '';
-
             res.on('data', (chunk) => { body += chunk; });
-
             res.on('end', () => { resolve(JSON.parse(body)); });
 
         });
-
         req.on('error', (err) => {
             console.error(err);
             reject(err);
@@ -43,22 +39,16 @@ function main() {
     const TeleBot = require('telebot');
     const token = readToken();
     const bot = new TeleBot(token);
-
     // Start polling to accept commands
     bot.start();
-
-
     // Simple greeting
     bot.on(['/start', '/hello'], (msg) => {
         msg.reply.text('Welcome! Use the inline function to find cards!');
     });
-
-
     // If used like @ygowikibot <query>
     bot.on('inlineQuery', (msg) => {
         // Create a new answer list object
-        const answers = bot.answerList(msg.id, { cacheTime: 60 });
-
+        const answers = bot.answerList(msg.id, { cacheTime: 1 });
         // Search for the given query
         let query = msg.query;
         return search(query).then((response) => {
@@ -66,35 +56,37 @@ function main() {
             if (!(Array.isArray(response) && response.length > 0)) {
                 return;
             }
-
             const items = response[0];
-
             // Show at most 10 items
-            let length = Math.min(10, items.length);
-
+            let length = Math.min(MAX_ITEMS, items.length);
             for (let i = 0; i < length; i++) {
                 // Extract fields from item
-                let id = items[i].id;
-                let name = items[i].name;
-                let url = PIC_URL + id + '.jpg';
-
+                let cur = items[i]
+                let id = cur.id;
+                let filename = id + '.jpg';
+                let url = PIC_URL + filename;
+                let small_url = PIC_SMALL_URL + filename;
+                let name = cur.name;
+                let type = cur.type;
+                let race = cur.race;
+                let desc = cur.desc;
+                let ban_tcg = cur.ban_tcg;
+                caption = name + '\n' + race + ' / ' + type + '\n\n' + desc + '\n\n' + 'Ban status: '
+                caption += ban_tcg ? ban_tcg : 'Unlimited'
                 // Generate photo object and prepare to get send
                 answers.addPhoto({
                     id: id,
-                    caption: name,
+                    caption: caption,
                     photo_url: url,
-                    thumb_url: url,
+                    thumb_url: small_url,
                 });
             }
-
             // DEBUG: Log answers
             console.log(answers);
-
             // Send all answers
             return bot.answerQuery(answers);
         });
     });
 }
-
 
 main();
